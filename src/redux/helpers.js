7d08e42c130bd0d65538b327adf11c738b7ef441
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-05-12 14:27:08 +0300

    fix bug

--- src/redux/helpers.js
+++ src/redux/helpers.js
@@ -9,7 +9,7 @@ export const fetchContacts = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await axios.get('/contacts');
-      return response.date;
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
