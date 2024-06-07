298d3669f10f6c1caf8af0f97d20c2b4d0f60130
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-06-07 06:58:40 +0300

    fix styling

--- src/components/ContactCard/ContactCard.styled.js
+++ src/components/ContactCard/ContactCard.styled.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const Item = styled.li`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
+  gap: 12px;
   padding-top: 12px;
   color: #000;
 `;
 
 export const Btn = styled.button`
-  width: 60px;
   border-radius: 10px;
   border: none;
   box-shadow: 0 1px 7px grey;
